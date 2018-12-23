18a9c607491dd3a3e707b6f4be1b05ed18513a69
Dylan Chong <dylan.chong@icloud.com>
2018-12-23 14:08:44 +1300

    Tidy Vim file

--- vim.py
+++ vim.py
@@ -2,18 +2,23 @@ from talon.voice import Word, Context, Key, Rep, Str, press
 
 ctx = Context('vim')
 ctx.keymap({
-    'align (par|paragraph)': 'gwip',
+    # Files
     'save': Key('escape : w enter'),
     'exit': Key(': q enter'),
     'force save': Key('escape : w ! enter'),
     'save exit':  Key('escape : w q enter'),
     'save all':   Key('escape : w a enter'),
-    'move line up': 'ddkP',
-    'move line down': 'ddp',
-    'change word': 'ciw',
-    'switch split': Key('ctrl-w ctrl-w'),
 
     # Tags
     '(jump deaf|jump to definition)': Key('ctrl-]'),
     'jump back': Key('ctrl-t'),
+
+    'align (par|paragraph)': 'gwip',
+
+    'move line up': 'ddkP',
+    'move line down': 'ddp',
+
+    'change word': 'ciw',
+
+    'switch split': Key('ctrl-w ctrl-w'),
 })
