5e737a9f784decff71968ffe08dda756956fb3ad
Dylan Chong <dylan.chong@icloud.com>
2019-05-02 15:53:20 +1200

    Add delay to sometimes fix keys getting typed out of order

---
+++ 0wait.py
@@ -0,0 +1,10 @@
+# TEMP Hack to remove keys getting typed out of order
+# TODO: remove this when the new api is out
+
+from talon import voice
+
+old_press = voice.press
+def press(key, **kwargs):
+    kwargs['wait'] = 4000
+    return old_press(key, **kwargs)
+voice.press = press
